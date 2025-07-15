47f902dee338565d586c35e25bc0d8099d1f28c0
drivebyluck <36115090+drivebyluck@users.noreply.github.com>
2025-07-15 15:17:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import { OpenAI } from 'openai';
 
 dotenv.config();
